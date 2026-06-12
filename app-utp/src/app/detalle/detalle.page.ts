00593fe35e46bfde1fee56a636ef41811d18ba50
C3s4r310 <cu046481@gmail.com>
2026-06-12 18:04:12 +0000

    Refactor: DetallePage

--- app-utp/src/app/detalle/detalle.page.ts
+++ app-utp/src/app/detalle/detalle.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ResumenAppComponent } from '../components/resumen-app/resumen-app.component';
+import { SelectorTipoComponent } from '../components/selector-tipo/selector-tipo.component';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.page.html',
   styleUrls: ['./detalle.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule, ResumenAppComponent, SelectorTipoComponent],
 })
-export class DetallePage implements OnInit {
+export class DetallePage {
+  tipo = 'Sin tipo seleccionado';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.tipo = params['tipo'] || 'Sin tipo seleccionado';
+    });
   }
 
+  actualizarTipo(nuevoTipo: string) {
+    this.tipo = nuevoTipo;
+  }
 }
