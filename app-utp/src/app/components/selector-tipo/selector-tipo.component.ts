a387464151ae1a26b21c7eb2e4083caa07c3a885
C3s4r310 <cu046481@gmail.com>
2026-06-12 18:13:46 +0000

    add components/selector-tipo

---
+++ app-utp/src/app/components/selector-tipo/selector-tipo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-tipo',
+  templateUrl: './selector-tipo.component.html',
+  styleUrls: ['./selector-tipo.component.scss'],
+})
+export class SelectorTipoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
