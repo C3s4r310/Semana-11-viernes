2a151fdbe557f96c1bb2cc67037ce24807cf06d5
C3s4r310 <cu046481@gmail.com>
2026-06-12 18:14:22 +0000

    refactor: App-routing

--- app-utp/src/app/app-routing.module.ts
+++ app-utp/src/app/app-routing.module.ts
@@ -4,22 +4,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () =>
-      import('./home/home.module').then(m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module')
+      .then(m => m.HomePageModule)
+  },
+  {
+    path: 'detalle',
+    loadChildren: () => import('./detalle/detalle.module')
+      .then(m => m.DetallePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'detalle',
-    loadChildren: () => import('./detalle/detalle.module').then( m => m.DetallePageModule)
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
